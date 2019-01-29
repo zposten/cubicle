225b43af1e91654907734e07a2573a381977dee7
Zach Posten <zposten@gmail.com>
2019-01-29 15:38:46 -0600

    feat: Add fake blog posts to the 'app'

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,25 @@ import Link from 'next/link'
 import styled from 'styled-components'
 import {Layout} from '../components/Layout'
 
+const PostLink = (props) => (
+  <li>
+    <Link 
+      href={`/post?title=${props.title}`}
+      as={`/p/${props.id}`}
+      >
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
+
 const Index = () => (
   <Layout>
     <Title>
       Hello Zach
     </Title>
-    <Link href='/about'>
-      <button>About me</button>
-    </Link>
+    <PostLink id='hello' title='Hello Next.js' />
+    <PostLink id='awesome' title='Learn how awesome Zach is' />
+    <PostLink id='ball' title='I can hit the ball far' />
   </Layout>
 )
 
