2a1c015d12c9a31ffa9e40df98f27b6fdf45467f
Zach Posten <zach@posten.io>
2020-02-05 11:06:14 -0600

    fix(blog): Display contextual titles on list pages

--- src/pages/blog/food/[meal]/index.js
+++ src/pages/blog/food/[meal]/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {useRouter} from 'next/router'
 import Head from 'next/head'
 
+import {Title} from 'components'
 import cards from 'content/cards'
 import {BlogCardList} from 'sections/blog'
 import {capitalize} from 'general/util'
@@ -10,10 +11,15 @@ export default function BlogMeal(props) {
   const router = useRouter()
   let meal = router.query.meal
   let mealCards = meal && cards.food[meal]
+  let title = `${capitalize(meal)} recipes!`
+
   return (
     <>
-      <Head>{meal && <title>{capitalize(meal)} recipes!</title>}</Head>
-      <BlogCardList cards={mealCards} router={props.router} />
+      <Head>{meal && <title>{title}</title>}</Head>
+
+      <BlogCardList cards={mealCards} router={props.router}>
+        <Title title={title} subtitle="My favorite, time-tested recipes" />
+      </BlogCardList>
     </>
   )
 }
--- src/sections/blog/BlogCardList.js
+++ src/sections/blog/BlogCardList.js
@@ -2,17 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {useRouter} from 'next/router'
 
-import {Title, Card, CardColumn} from 'components'
+import {Card, CardColumn} from 'components'
 import {PageLayout} from 'components/layout'
 
 export function BlogCardList(props) {
   const router = useRouter()
   return (
     <PageLayout>
-      <Title
-        title="Blog"
-        subtitle="My thoughts, views, and opinions on technical and non-technical matters"
-      />
+      {props.children}
       <CardColumn>
         {props.cards &&
           props.cards.map(c => (
